e52da8f8467096578a0542823ebc514ae9b1cc86
Nicolas Massé <nicolas.masse@itix.fr>
2024-03-14 14:59:16 +0100

    wip

--- jetson-orin-nano/prepare-kickstart.sh
+++ jetson-orin-nano/prepare-kickstart.sh
@@ -9,11 +9,11 @@ fi
 declare -a ALL_CONFIG_VARS=(KICKSTART_WIFI_SSID KICKSTART_WIFI_WPA_KEY KICKSTART_ADMIN_USERNAME KICKSTART_ADMIN_SSH_KEY KICKSTART_ADMIN_PASSWORD KICKSTART_OSTREE_URL KICKSTART_OSTREE_REF KICKSTART_MICROSHIFT_PULL_SECRET)
 if [ -z "${KICKSTART_WIFI_SSID:-}" ]; then read -p "Wifi SSID: " KICKSTART_WIFI_SSID; fi
 if [ -z "${KICKSTART_WIFI_WPA_KEY:-}" ]; then read -p "Wifi WPA Key: " KICKSTART_WIFI_WPA_KEY; fi
-if [ -z "${KICKSTART_ADMIN_USERNAME:-}" ]; then read -p "Admin Username: " KICKSTART_ADMIN_USERNAME; fi
-if [ -z "${KICKSTART_ADMIN_SSH_KEY:-}" ]; then read -p "Admin SSH key: " KICKSTART_ADMIN_SSH_KEY; fi
 if [ -z "${KICKSTART_OSTREE_URL:-}" ]; then read -p "Ostree repository URL: " KICKSTART_OSTREE_URL; fi
 if [ -z "${KICKSTART_OSTREE_REF:-}" ]; then read -p "Ostree reference: " KICKSTART_OSTREE_REF; fi
 if [ -z "${KICKSTART_MICROSHIFT_PULL_SECRET:-}" ]; then read -p "Microshift Pull Secret: " KICKSTART_MICROSHIFT_PULL_SECRET; fi
+if [ -z "${KICKSTART_ADMIN_USERNAME:-}" ]; then read -p "Admin Username: " KICKSTART_ADMIN_USERNAME; fi
+if [ -z "${KICKSTART_ADMIN_SSH_KEY:-}" ]; then read -p "Admin SSH key: " KICKSTART_ADMIN_SSH_KEY; fi
 if [ -z "${KICKSTART_ADMIN_PASSWORD:-}" ]; then
   read -s -p "Admin Password: " password
   KICKSTART_ADMIN_PASSWORD="$(echo -n "$password" | mkpasswd -sm sha512crypt)"
@@ -29,7 +29,7 @@ for i in "${ALL_CONFIG_VARS[@]}"; do
 done
 
 for template_file in kickstarts/*.cfg.template; do
-  echo "$template_file -> $kickstart_file"
   kickstart_file="$(basename "$template_file" .template)"
+  echo "$template_file -> $kickstart_file"
   envsubst "$shell_format" < "$template_file" > "$kickstart_file"
 done
